4f2171c1d0f80d6684b88d164a289ddfd3589efc
1346489 <3945142752@qq.com>
2026-08-27 16:08:02 +0800

    Update MainActivity.java

--- app/src/main/java/com/moe/nyanhelper/MainActivity.java
+++ app/src/main/java/com/moe/nyanhelper/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
